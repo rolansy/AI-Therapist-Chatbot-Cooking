06aa7c04b90f135bee46caa6bb52a73e4d45a310
rolansy <ronalshoeygeorge@gmail.com>
2024-10-25 22:51:45 +0530

    Refactor bot message text and variable names

--- script.js
+++ script.js
@@ -25,15 +25,15 @@ function sendMessage() {
         .then(data => {
             const botMessage = document.createElement('div');
             if (data.organic_results && data.organic_results.length > 0) {
-                botMessage.textContent = `Bot: ${data.organic_results[0].snippet}`;
+                botMessage.textContent = `Ron: ${data.organic_results[0].snippet}`;
             } else {
-                botMessage.textContent = `Bot: Sorry, I couldn't find any results.`;
+                botMessage.textContent = `Ron: Sorry, I couldn't find any results.`;
             }
             chatLog.appendChild(botMessage);
         })
         .catch(error => {
             const botMessage = document.createElement('div');
-            botMessage.textContent = `Bot: Sorry, I couldn't fetch the data.`;
+            botMessage.textContent = `Ron: Sorry, I couldn't fetch the data.`;
             chatLog.appendChild(botMessage);
         });
 
